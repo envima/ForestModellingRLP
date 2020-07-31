45bb86a64348f1b31f31d23f49eb50e3d11651f5
Ludwigm6 <marvin.ludwig93@gmail.com>
2020-07-31 12:22:24 +0200

    ffs models

--- script/ffsmodel_meta_classes_main_trees.R
+++ script/ffsmodel_meta_classes_main_trees.R
@@ -1,4 +1,5 @@
 ##37677
+## Modelling of the main tree classes for RLP
 
 rm(list=ls())
 
@@ -7,69 +8,52 @@ library(caret)
 library(foreach)
 library(doParallel)
 library(CAST)
-library(ranger)
 library(randomForest)
-##data
+
+## choose model response
 response_type<-"meta_classes_main_trees"
 
+# load modelling data
 pred_resp<-readRDS(paste0("data/modelling/", response_type, ".RDS"))
-summary_data<-readRDS("data/modelling/summary_train_data.RDS")
+
+
+### Initialise Leave-Location out cv
+## Main difference in the modelling strategy: we combine multiple location in the folds
+# sample a ten fold cv stratified after the main tree species (BAGRu)
+# spacevar = "FAT__ID" divides the polygon IDs into different folds
+# CAST version 0.4.2
+indices <- CreateSpacetimeFolds(pred_resp, spacevar = "FAT__ID", k=10, class = "BAGRu")
 
 
 
-
-#k <- summary_data$number_of_distinct_locations[summary_data$data_set==response_type][1] # Anzahl der BufferID's pro Landnutzungsklasse
-#uniqueSites <- unique(pred_resp[,c("FAT__ID","BAGRu")])
-#uniqueSites$ID <- 1:nrow(uniqueSites)
-
-
-# create folds with one location for each tree species
-#folds <- createFolds(uniqueSites$BAGRu,k,list=TRUE)
-
-#for (i in 1:length(folds)){
-#  uniqueSites$Group[uniqueSites$ID%in%folds[[i]]] <- paste0("Group_",i)
-#}
-
-#pred_resp <- merge(pred_resp,uniqueSites,by.x="FAT__ID",by.y="FAT__ID")
-
-### Index erstellen:
-
-indices <- CreateSpacetimeFolds(pred_resp, spacevar = "FAT__ID",k=10, class = "BAGRu")
-#table(pred_resp$BAGRu[indices$indexOut[[5]]],pred_resp$FAT__ID[indices$indexOut[[5]]] )
-
-####
-
-
+### Initialize Modelling
 
 set.seed(10)
 ctrl <- trainControl(method="cv",index = indices$index,
                      savePredictions=TRUE )
-#load and prepare dataset:
 
-#create folds for Leave Location Out Cross Validation:
 
+
+# no model tuning
 tgrid <- expand.grid(.mtry = 2,
                      .splitrule = "gini",
                      .min.node.size = 1)
+
 
 predictors <- pred_resp[,2:145]
 response <- factor(pred_resp$BAGRu)
 
 #run ffs model with Leave Location out CV
+# we use randomForest now, ranger defaults to num.threads = number of CPUs available
+# we dont want to mess with double parallel
 cl <- makeCluster(10)
 registerDoParallel(cl)
 set.seed(10)
 ffsmodel <- ffs(predictors,response, metric="Kappa", method="rf",
-                trControl=ctrl, importance = TRUE ,tuneLength =  1, ntree = 50)
-
-mod <- train(x = predictors, y = response, method = "ranger", metric="Kappa", 
-             trControl=ctrl, importance="impurity", tuneGrid = tgrid, num.trees = 50)
-mod
-
+                trControl=ctrl, importance = TRUE ,tuneLength = 1, ntree = 50)
 ffsmodel
-
+varImp(ffsmodel)
 saveRDS(ffsmodel,paste0(response_type,"_ffs.RDS"))
-
 
 stopCluster(cl)
 #
--- script/ffsmodel_quality.R
+++ script/ffsmodel_quality.R
@@ -4,63 +4,61 @@ rm(list=ls())
 
 
 library(caret)
-#library(foreach)
-#library(doParallel)
+library(foreach)
+library(doParallel)
 library(CAST)
 
 ##data
 dat_lst<-list.files("data/modelling/", pattern="quality", full.names = TRUE)
 
+
 for(i in dat_lst){
-response_type<- gsub(".RDS", "",i)
-response_type<-gsub("data/modelling/", "",response_type)
-print(response_type)
-pred_resp<-readRDS(i)
-dens_plots<-ddply(pred_resp,~Quality,summarise,number_of_distinct_locations=length(unique(FAT__ID)))
+
+  
+  response_type = gsub(".RDS", "",basename(i))
+  
+  print(response_type)
+  pred_resp<-readRDS(i)
+  ### Initialise Leave-Location out cv
+  ## Main difference in the modelling strategy: we combine multiple location in the folds
+  # sample a ten fold cv stratified after the main tree species (BAGRu)
+  # spacevar = "FAT__ID" divides the polygon IDs into different folds
+  # CAST version 0.4.2
+  indices <- CreateSpacetimeFolds(pred_resp, spacevar = "FAT__ID", k=10, class = "Quality")
+
+  
+  
+  ### Initialize Modelling
+  
+  set.seed(10)
+  ctrl <- trainControl(method="cv",index = indices$index,
+                       savePredictions=TRUE )
+  
+  
+  
+  # no model tuning
+  tgrid <- expand.grid(.mtry = 2,
+                       .splitrule = "gini",
+                       .min.node.size = 1)
 
 
 
-
-k <- dens_plots$number_of_distinct_locations[1] # Anzahl der BufferID's pro Landnutzungsklasse
-print(k)
-uniqueSites <- unique(pred_resp[,c("FAT__ID","BAGRu")])
-uniqueSites$ID <- 1:nrow(uniqueSites)
-folds <- createFolds(uniqueSites$BAGRu,k,list=TRUE)
-for (i in 1:length(folds)){
-  uniqueSites$Group[uniqueSites$ID%in%folds[[i]]] <- paste0("Group_",i)
-}
-pred_resp <- merge(pred_resp,uniqueSites,by.x="FAT__ID",by.y="FAT__ID")
-### Index erstellen:
-
-indices <- CreateSpacetimeFolds(pred_resp,spacevar = "Group",k=k)
-
-
-
-
-####
-
-cl <- makeCluster(k)
-registerDoParallel(cl)
-
-set.seed(10)
-ctrl <- trainControl(method="cv",index = indices$index,
-                     savePredictions=TRUE )
-#load and prepare dataset:
-
-#create folds for Leave Location Out Cross Validation:
-
-tgrid <- expand.grid(.mtry = seq(2, 32, by=3),
-                     .splitrule = "gini",
-                     .min.node.size = 1)
-
-predictors <- pred_resp[,2:145]
-response <- factor(pred_resp$BAGRu.x)
+  predictors <- pred_resp[,2:145]
+  response <- factor(pred_resp$Quality)
 
 #run ffs model with Leave Location out CV
+#run ffs model with Leave Location out CV
+# we use randomForest now, ranger defaults to num.threads = number of CPUs available
+# we dont want to mess with double parallel
+cl <- makeCluster(10)
+registerDoParallel(cl)
 set.seed(10)
-ffsmodel <- ffs(predictors,response,metric="Kappa",method="ranger",
-                trControl=ctrl, importance="impurity", tuneGrid = tgrid)
+ffsmodel <- ffs(predictors,response, metric="Kappa", method="rf",
+                trControl=ctrl, importance = TRUE ,tuneLength = 1, ntree = 50)
 ffsmodel
+varImp(ffsmodel)
+
+
 
 saveRDS(ffsmodel,paste0(response_type,"_ffs.RDS"))
 
