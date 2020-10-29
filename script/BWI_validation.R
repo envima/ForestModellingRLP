517870d02e32ad559e9dc3426560feeb63bf9125
Ludwigm6 <marvin.ludwig93@gmail.com>
2020-10-29 15:08:06 +0100

    external validation

--- script/BWI_validation.R
+++ script/BWI_validation.R
@@ -28,14 +28,35 @@ library(caret)
 #--------------------------------------
 
 BWI <- sf::read_sf("data/BWI/BWI_validation.shp")
-pred <- raster::raster("prediction/diverse_trees_pred.grd")
+pred <- raster::raster("data/prediction/main_trees_pred.grd")
 BWI <- sf::st_transform(BWI, crs(pred))
 
+aoa = raster::raster("data/aoa_main/aoa_main.grd", 2)
 
 # 3 - confusion matrix ####
 #--------------------------
 # extract predicted values for each polygon of BWI
-pred_extr <- raster::extract(pred, BWI)
+pred_extr <- raster::extract(pred, BWI, df = TRUE)
+
+BWI$ID = seq(nrow(BWI))
+
+pred_extr = merge(st_drop_geometry(BWI), pred_extr, by = "ID")
+pred_extr = na.omit(pred_extr)
+
+valid = data.frame(BWI = pred_extr$Pred_No, pred = pred_extr$layer)
+
+validConf = valid[valid$BWI %in% seq(5),]
+confusionMatrix(table(validConf))
+pred_extr$aoa = aoa_extr$AOA
+
+
+# extract aoa
+
+aoa_extr = raster::extract(aoa, BWI, df = TRUE)
+aoa_extr = merge(st_drop_geometry(BWI), aoa_extr, by = "ID")
+aoa_extr = na.omit(aoa_extr)
+
+
 pred_extr <- as.data.frame(do.call(rbind,pred_extr))
 
 # Dataframe for each true pixel one predicted pixel 
---
+++ script/validation_left_out_polygons.R
@@ -0,0 +1,96 @@
+library(sf)
+library(tidyverse)
+
+`%not_in%` <- purrr::negate(`%in%`)
+
+# relevant class information from original polygons
+polygons = st_read("data/Trainingsgebiete_RLP/Etb_Qua_Dim_Rei_WGS84.gpkg") %>% st_drop_geometry()
+polygons = polygons[,c("FAT__ID", "Phase", "BAGRu")]
+
+# attach relevant class information to full extraction set
+# format properly
+rlp_extract = readRDS("data/model_training_data/RLP_extract.RDS")
+rlp_extract = merge(rlp_extract, polygons, by = "FAT__ID")
+rlp_extract$surface_intensity_mean = NULL
+rlp_extract$ID = NULL
+rlp_extract$Quality = paste0(rlp_extract$BAGRu, "_", rlp_extract$Phase)
+
+
+
+# validation for each model
+models = c("meta_classes_main_trees", "meta_classes_diverse")
+
+for(m in models){
+  meta = list()
+  training_set = readRDS(paste0("data/model_training_data/", m,".RDS"))
+  
+  meta$'Model' = m
+  meta$'Number of training polygons' = length(unique(training_set$FAT__ID))
+  meta$'Number of training pixel per class' = as.list(table(training_set$BAGRu))
+  
+  rlp_extract_sub = rlp_extract %>% filter(BAGRu %in% unique(training_set$BAGRu)) %>% filter(FAT__ID %not_in% training_set$FAT__ID)
+  rlp_extract_sub = na.omit(rlp_extract_sub)
+  
+  meta$'Number of validation polygons' = length(unique(rlp_extract_sub$FAT__ID))
+  meta$'Number of validation pixel per class' = as.list(table(rlp_extract_sub$BAGRu))
+  
+  
+  # load model
+  mod = readRDS(paste0("data/models/", m,"_ffs.RDS"))
+  valid = stats::predict(object = mod, newdata = rlp_extract_sub)
+  
+  val_df = data.frame(FAT__ID = rlp_extract_sub$FAT__ID,
+                      Observed = rlp_extract_sub$BAGRu, 
+                      Predicted = valid)
+  
+  val_cm = confusionMatrix(table(val_df[,2:3]))
+  
+  
+  # output
+  
+  saveRDS(val_df, paste0("data/validation/", m, "_validation_df.RDS"))
+  saveRDS(val_cm, paste0("data/validation/", m, "_confusionmatrix.RDS"))
+  yaml::write_yaml(yaml::as.yaml(meta), file = paste0("data/validation/", m, "_meta.yaml"))
+}
+
+
+
+# Quality models
+
+models = c("quality_beech", "quality_doug", "quality_larch", "quality_lbk",
+           "quality_lbl", "quality_oak", "quality_pine", "quality_spruce")
+
+for(m in models){
+  meta = list()
+  training_set = readRDS(paste0("data/model_training_data/", m,".RDS"))
+  
+  meta$'Model' = m
+  meta$'Number of training polygons' = length(unique(training_set$FAT__ID))
+  meta$'Number of training pixel per class' = as.list(table(training_set$Quality))
+  
+  rlp_extract_sub = rlp_extract %>% filter(Quality %in% unique(training_set$Quality)) %>% filter(FAT__ID %not_in% training_set$FAT__ID)
+  rlp_extract_sub = na.omit(rlp_extract_sub)
+  
+  meta$'Number of validation polygons' = length(unique(rlp_extract_sub$FAT__ID))
+  meta$'Number of validation pixel per class' = as.list(table(rlp_extract_sub$Quality))
+  
+  
+  # load model
+  mod = readRDS(paste0("data/models/", m,"_ffs.RDS"))
+  valid = stats::predict(object = mod, newdata = rlp_extract_sub)
+  
+  val_df = data.frame(FAT__ID = rlp_extract_sub$FAT__ID,
+                      Observed = rlp_extract_sub$Quality, 
+                      Predicted = valid)
+  
+  val_cm = confusionMatrix(table(val_df[,2:3]))
+  
+  
+  # output
+  
+  saveRDS(val_df, paste0("data/validation/", m, "_validation_df.RDS"))
+  saveRDS(val_cm, paste0("data/validation/", m, "_confusionmatrix.RDS"))
+  yaml::write_yaml(yaml::as.yaml(meta), file = paste0("data/validation/", m, "_meta.yaml"))
+}
+
+
