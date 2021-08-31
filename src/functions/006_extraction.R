64b96d5d37992794fc2d62ec42be55bbb4cea51e
Lisa Bald <57003377+Baldl@users.noreply.github.com>
2021-08-31 17:01:16 +0200

    add lidar

--- src/functions/006_extraction.R
+++ src/functions/006_extraction.R
@@ -15,15 +15,21 @@ extraction <- function(rasterStack, pol, bufferSize = -20, idColName = "FAT__ID"
     pol = st_buffer(pol, dist = bufferSize)
     pol = pol[!st_is_empty(pol),]
   }
-  
-  
+
   # extract all polygons from raster stack
   result = lapply(seq(nrow(pol)), function(i){
     cur = pol[i,]
     ext <- terra::ext(cur)
     
+<<<<<<< HEAD
+    LIDARIndices <- rasterdb$raster(ext)
+    LIDARIndices = dropLayer(LIDARIndices, "band1") 
+    #all = terra::crop(rasterStack, ext)
+    chm = LIDARIndices$chm_height_max
+=======
     
     chm = rasterStack$chm_height_max
+>>>>>>> 7b7e914c6a096582107e0dcb323a3aa1e87370b6
     chm = terra::extract(chm, vect(cur), df = TRUE)
     
     # check if polygon only contains one row
@@ -39,7 +45,13 @@ extraction <- function(rasterStack, pol, bufferSize = -20, idColName = "FAT__ID"
       return("Luxemburg")
     } else {
       
+<<<<<<< HEAD
+      sen = terra::crop(rasterStack, ext)
+      all = rast(list(LIDARIndices, sen))
+      
+=======
       all = terra::crop(rasterStack, ext)
+>>>>>>> 7b7e914c6a096582107e0dcb323a3aa1e87370b6
       
       df = terra::extract(all, vect(cur), df = TRUE)
       df = df %>% dplyr::mutate(cur %>% select((!!sym(idColName))))
