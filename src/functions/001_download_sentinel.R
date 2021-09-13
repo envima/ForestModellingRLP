70062911f84d1ad2b85b619667ad400f9cc24015
Baldl <lisabald@t-online.de>
2021-09-13 16:34:39 +0200

    hansen

--- src/functions/001_download_sentinel.R
+++ src/functions/001_download_sentinel.R
@@ -48,40 +48,43 @@ download_sentinel = function(startdate = "2019-06-29",
   # select bands 
   img = img$select(band_names)
   
-  continue = readline("Do you want to download all the sentinel tiles listed above?[TRUE/FALSE]")  
   
-  if (continue == TRUE) {
+  img2 = img$mosaic()$reproject("EPSG:25832")
+  
+  #continue = readline("Do you want to download all the sentinel tiles listed above?[TRUE/FALSE]")  
+  
+  #if (continue == TRUE) {
     #img$getInfo()
     
     
-    img_02 = ee_imagecollection_to_local(
-      ic = img,
-      dsn = outfilePath,
-      region = region,
-      crs = 'EPSG:25832',
-      via = "drive",
-      container = "rgee_backup",
-      maxPixels = 1e+09,
-      lazy = FALSE,
-      public = FALSE,
-      add_metadata = TRUE,
-      timePrefix = TRUE,
-      quiet = FALSE,
-      scale = 20
-    )
-  } # end if 
-  if (continue == FALSE) {
+   # img_02 = ee_imagecollection_to_local(
+    #  ic = img,
+     # dsn = outfilePath,
+      #region = region,
+      #crs = 'EPSG:25832',
+      #via = "drive",
+      #container = "rgee_backup",
+      #maxPixels = 1e+09,
+      #lazy = FALSE,
+      #public = FALSE,
+      #add_metadata = TRUE,
+      #timePrefix = TRUE,
+      #quiet = FALSE,
+      #scale = 20
+    #)
+  #} # end if 
+  #if (continue == FALSE) {
     #------------------------
-    continue = readline("Do you want to download one specific tile?[y/n]")  
-    if (continue == "y" | continue == "Y") {
-      nTile = readline("Which one?[number of tile in list above]")
+  #  continue = readline("Do you want to download one specific tile?[y/n]")  
+   # if (continue == "y" | continue == "Y") {
+    #  nTile = readline("Which one?[number of tile in list above]")
       
       #img2 = img$filterMetadata("DATATAKE_IDENTIFIER" , "equals", "20190224T103019_20190224T103021_T32UNV")
-      img2 =ee$Image(tiles$id[[as.integer(nTile)]])$reproject("EPSG:25832")
+     # img2 =ee$Image(tiles$id[[as.integer(nTile)]])$reproject("EPSG:25832")$select(band_names)
       
       
       task_img <- ee_image_to_drive(
-        image = img,
+        image = img2,
         folder = "rgee_backup",
         scale = 20,
         region = region,
@@ -92,6 +95,7 @@ download_sentinel = function(startdate = "2019-06-29",
       ee_monitoring(task_img)
       # Move results from Drive to local
       ee_drive_to_local(task = task_img, dsn = outfilePath)
+      
     }
-  }
-}
+ # }
+#}
